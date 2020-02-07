4f94e856f024a579fb7a2477b4e540143e668afb
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-06 19:49:45 -0800

    Check leap year

---
+++ Assignment06FEB/LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test to make sure that my leap year logic is correct
+            
+            int count = 0;
+
+            for (int y = 1730; y < 2004; y++)
+            {
+                if (y % 400 == 0)
+                {
+                    Console.WriteLine(y);
+                    count += 1;
+                }
+                else if ((y % 4 == 0) && (y % 100 != 0))
+                {
+                    Console.WriteLine(y);
+                    count +=1;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
